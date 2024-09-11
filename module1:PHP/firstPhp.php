8d5a85a2e7e96a64d197ae8f4ff2d0560297633f
villethan720 <93165624+villethan720@users.noreply.github.com>
2024-09-11 10:24:15 -0700

    Add files via upload

---
+++ module1:PHP/firstPhp.php
@@ -0,0 +1,6 @@
+<?php
+echo "this is my first line of php code";
+echo "this is my second line of php code";
+
+
+?>
---
+++ module1:PHP/webpage.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="/stylesheet/style.css">
+        <title>Gus Art</title>
+        <h1>Welcome to Gus Art</h1>
+
+    </head>
+
+    <body>
+        <!--add Php to the body-->
+        <?php
+            $name = "Gus";
+            $pants = "Star Road";
+            $jacket = "Star Coat";
+            $crew = "Star Gang";
+        ?>
+
+        <h1>My name is <?php echo $name ?> and I am the creator</h1>
+        <p>Check out some of my pieces</p>
+        <br>
+
+        <img src="img/starPants1.jpeg">
+        <img src="img/starPants2.jpeg">
+        <p1>This is the first piece <?php echo $pants ?></p1>
+
+        <br>
+        <br>
+        <img src="img/starCoat1.jpeg">
+        <img src="img/starCoat2.jpeg">
+        <p1>This is the second piece <?php echo $jacket ?></p1>
+        
+        <br>
+        <p2>If you are wondering how to style it, then check these pics out</output></p2>
+
+
+        <p></p>
+    </body>
+</html>
